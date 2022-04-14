6f81e000b33c858a34661440d15114e4c7120d08
Mateo <madiazt@unal.edu.co>
2022-04-13 19:07:23 -0500

    added log-out functionality

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+
+// mui
 import LoginSharpIcon from '@mui/icons-material/LoginSharp';
+import LogoutSharpIcon from '@mui/icons-material/LogoutSharp';
 import MenuIcon from '@mui/icons-material/Menu';
 import { 
     Container,
@@ -16,7 +19,9 @@ import {
 
 // redux state management
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { selectUser } from "./features/userSlice";
+import { logout } from "./features/userSlice";
 
 // Using Inline Styling
 
@@ -27,11 +32,21 @@ export const Header = () => {
 
     const [anchorNav, setAnchorNav] = useState(null)
 
+    const dispatch = useDispatch();
+
     const handleOpenMenu = (e) => {
         setAnchorNav(e.currentTarget);
     };
     const handleCloseMenu = (e) => {
         setAnchorNav(null);
+    }
+
+    const handleLogout = (e) => {
+        handleCloseMenu();
+        // erase token
+        // set to null redux user value
+        localStorage.removeItem("deb-token");
+        dispatch(logout());
     }
     if (!user) {
         return(
@@ -99,7 +114,55 @@ export const Header = () => {
         )
     } else {
         return (
-            <div>logedin</div>
+            <AppBar position="static" color="success">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/" style={{ color: "inherit", textDecoration: 'none' }}>
+                            {title}
+                        </Link>
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 0 }} >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorNav}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right'
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right'
+                            }}
+                            open={Boolean(anchorNav)}
+                            onClose={handleCloseMenu}
+                        >
+                            <MenuItem onClick={handleLogout}>
+                                    <Stack direction="row" alignItems="center" gap={1}>
+                                        <LogoutSharpIcon color="success"/>    
+                                        <Typography textAlign="center">
+                                            Log-out
+                                        </Typography>
+                                    </Stack>
+                            </MenuItem>
+                        </Menu>
+                    </Box >
+                    </Toolbar>
+                </Container>
+            </AppBar>
         )
     }
 }
