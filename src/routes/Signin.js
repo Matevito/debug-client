4df6d1135a48d35fef25a965689d8de7ddd198ab
Mateo <madiazt@unal.edu.co>
2022-04-13 19:38:46 -0500

    added handleValues functions on login comp

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Signin = () => {
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+    const [repPassword, setrepPassword] = useState("");
+    const [email, setemail] = useState("");
+
+    // component functions
+    const handleUsername = (e) => {
+        setusername(e.target.value)
+    };
+    const handlePassword = (e) => {
+        setpassword(e.target.value)
+    };
+    const handleRepPassword = (e) => {
+        setrepPassword(e.target.value)
+    };
+    const handleEmail = (e) => {
+        setemail(e.target.value)
+    };
+
+    // error and api res functions
+    
     return (
         <div>
             signin
