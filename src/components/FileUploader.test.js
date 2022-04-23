a814eaf9f5ef8c14d856d476184aef9b25df8c0a
Mateo <madiazt@unal.edu.co>
2022-04-22 21:47:01 -0500

    added vasic FileUploader test-suite

--- src/components/FileUploader.test.js
+++ src/components/FileUploader.test.js
@@ -1,5 +1,43 @@
+import React from "react";
+
+// testing utilities
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+// test component
+import { FileUploader } from "./FileUploader";
+
 describe("FileUploader component", () => {
-    test.todo("renders working upload btn")
-    test.todo("comp renders screenshots names");
-    test.todo("comp handles deleteFile function");
+    
+    test("renders upload btn", () => {
+        render(
+            <FileUploader 
+                screenshots={[]}
+
+            />
+        )
+        expect(screen.getByText("Upload")).toBeInTheDocument();
+    })
+    test("comp renders screenshots names", () => {
+        const mock_screenshots = [
+            {
+                name: "first_image.jpeg"
+            },
+            {
+                name: "second_image.div"
+            },
+            {
+                name: "third_image.png"
+            }
+        ];
+        render(
+            <FileUploader 
+                screenshots={mock_screenshots}
+            />
+        );
+        expect(screen.getByText("first_image.jpeg")).toBeInTheDocument();
+        expect(screen.getByText("second_image.div")).toBeInTheDocument()
+        expect(screen.getByText("third_image.png")).toBeInTheDocument()
+    });
+    //test.todo comp handles deleteFile function
 })
