fa5db6ba8d7bc20922d28de7e974d0d435d7238c
Mateo <madiazt@unal.edu.co>
2022-04-24 19:53:11 -0500

    added basic layer of route protection

--- src/routes/ProjectGet.js
+++ src/routes/ProjectGet.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+
+// redux state management
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+
+// mui components
+
+// app components
+
+// api comp
+import api from "../features/api";
 
 export const ProjectGet = () => {
-    return (
-        <div>
-            display project info
-        </div>
-    )
+    const projectId = useParams().id;
+    const user = useSelector(selectUser);
+    let navigate = useNavigate();
+
+    const [authorized, setAuthorized] = useState(null);
+    const [projectInfo, setProjectInfo] = useState({});
+
+    useEffect(() => {
+        async function getProjectInfo() {
+            // get project info...
+            const config = {
+                headers: {
+                    "auth-token": user.token
+                }
+            }
+            try {
+                const projectRes = await api.get(`/project/${projectId}`, config);
+                setProjectInfo(projectRes.data)
+                setAuthorized(true);
+            } catch (err) {
+                setAuthorized(false);
+            }
+            
+        };
+        if (user) {
+            getProjectInfo();
+        }
+    }, [user, projectId])
+    if (!user) {
+        navigate("/");
+    } else if (authorized === null) {
+        // loading page...
+        return (
+            <div>Loading ...</div>
+        )
+    } else if (authorized === false ) {
+        navigate("/protected-route");
+    } else {
+
+        return (
+            <div>  {projectInfo.data.title}</div>
+        )
+    }
 }
