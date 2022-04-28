56423c1c97d9478a16a01a64dd2955d5cf4ebd21
Mateo <madiazt@unal.edu.co>
2022-04-28 16:52:46 -0500

    added basic skeleton of error routes

--- src/routes/Code404.js
+++ src/routes/Code404.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+// mui components
+import { 
+    Box,
+    Typography
+} from "@mui/material"
+import ReportGmailerrorredRoundedIcon from '@mui/icons-material/ReportGmailerrorredRounded';
+
 export const Code404 = () => {
     return (
-        <div>
-            404
-        </div>
+        <Box
+        fullWidth
+            sx={{
+                margin: "30px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+        >
+            <ReportGmailerrorredRoundedIcon fontSize="large"/>
+            <Typography variant="h1">404 ERROR!</Typography>
+            <Typography variant="span">
+                The page you are searching does not exist! If you think it should, comunicate with an admin.
+            </Typography>
+        </Box>
     )
 }
--- src/routes/Protected.js
+++ src/routes/Protected.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+// mui components
+import { 
+    Box,
+    Typography
+} from "@mui/material"
+import SecurityRoundedIcon from '@mui/icons-material/SecurityRounded';
+
 export const Protected = () => {
     return (
-        <div>
-            protected route
-        </div>
+        <Box
+        fullWidth
+            sx={{
+                margin: "30px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+        >
+            <SecurityRoundedIcon fontSize="large" />
+            <Typography variant="h1">Protected route</Typography>
+            <Typography variant="span">
+                You don't have the credentials or the user role to access this site.
+                Check with an admin if you think you do!
+            </Typography>
+        </Box>
     )
 }
