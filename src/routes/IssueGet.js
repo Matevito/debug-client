e87cdc30d41e07af3a354413fd023999c262ce64
Mateo <madiazt@unal.edu.co>
2022-04-29 18:25:24 -0500

    added skeleton of issueGet test-suite

--- src/routes/IssueGet.js
+++ src/routes/IssueGet.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+
+// redux state management
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+
+// mui components
+
+// app components
+import { LoadingPage } from "../components/LoadingPage";
+
+// api comp
+import  api from "../features/api";
 
 export const IssueGet = () => {
-    return (
-        <div>
-            issue get!!!
-        </div>
-    )
+    const user = useSelector(selectUser);
+    const issueId = useParams().id;
+    let navigate = useNavigate()
+
+    // state values;
+    const [issueInfo, setIssueInfo] = useState(null);
+    const [authorized, setAuthorized] = useState(null)
+
+    // set up issueInfo state value
+    useEffect(() => {
+        async function getIssueInfo() {
+            const config = {
+                headers: { "auth-token" : user.token }
+            };
+
+            try {
+                const issueRes = await api.get(`/issue/${issueId}`, config);
+                setIssueInfo(issueRes.data.data);
+                setAuthorized(true)
+            } catch (err) {
+                setAuthorized(false);
+                if (err.status === 401 || err.response.status === 401 ) {
+                    navigate("/protected-route")
+                } else{ 
+                    navigate("/404")
+                }
+            }
+        }
+        if (user) {
+            getIssueInfo()
+        }
+    }, [user, issueId])
+
+    if (!user) {
+        navigate("/")
+    } else if (authorized === null) {
+        return (
+            <LoadingPage />
+        )
+    } else if (authorized === true) {
+        return (
+            <div>
+                issue get!!!
+            </div>
+        )
+    }
 }
--- src/routes/IssueGet.test.js
+++ src/routes/IssueGet.test.js
@@ -1,4 +1,91 @@
+import { Provider } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+// testing comps
+import { render, screen, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+// test comp
+import { IssueGet } from "./IssueGet";
+
+// mock functions
+import { getData } from "../features/mockedUserStores";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const rootAPI = "https://pure-falls-26749.herokuapp.com/apiv1";
+const issueResponse = {
+    data: { issue: {
+        _id: "issueTestId",
+        title:" issue ticket",
+        description: "here goes a long and detailed description...",
+        project: "projectTestId",
+        status: "open",
+        priority: "high",
+        type: "feature req",
+        handlingTeam: [],
+        date: "2022-04-23T22:56:09.627Z",
+        screenshots: [],
+        v__: 0
+    }}
+}
+
+const server = setupServer(
+    rest.get(`${rootAPI}/issue/issueTestId`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(issueResponse))
+    })
+);
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
+const renderComponent = (url, store) => {
+    return render(
+        <Provider store={store}>
+            <MemoryRouter initialEntries={[url]}>
+                <Routes>
+                    <Route 
+                        path="/issue/:id"
+                        element={<IssueGet/>}
+                    />
+                </Routes>
+            </MemoryRouter>
+        </Provider>
+    )
+} 
 describe("IssueGet component", () => {
-    test.todo("protection routes test");
+    // test requirements!
+    let noLoggedUser;
+    let devStore;
+    let adminStore;
+    let userNotPartOfIssue;
+    const componentURL = "/issue/issueTestId";
+
+    beforeEach(() => {
+        const usersData = getData();
+        noLoggedUser = usersData[0];
+        devStore = usersData[1];
+        adminStore = usersData[3];
+        userNotPartOfIssue = usersData[2];
+    })
+    // required to mock rest-api server
+    beforeAll(() => server.listen())
+    afterEach(() => server.resetHandlers());
+    afterAll(() => {
+        server.close()
+        jest.clearAllMocks();
+    })
+
+    //auth tests
+    test("handles no user set-up", () => {
+        renderComponent(componentURL, noLoggedUser);
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/")
+    });
+    test.todo("handles if a user does not have access to the page");
+    test.todo("the issue called does not exist");
+
+    //render tests
     test.todo("render component tests")
 })
