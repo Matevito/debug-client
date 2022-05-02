33958eb10692cdcb579b7f31a0a964bafaf89e46
Mateo <madiazt@unal.edu.co>
2022-05-02 16:11:40 -0500

    added skeleton of comments displayer on IssueGet comp

--- src/components/IssueComments.js
+++ src/components/IssueComments.js
@@ -10,6 +10,15 @@ import {
     Chip,
     CircularProgress,
     Alert,
+    TableContainer,
+    Table,
+    TableHead,
+    TableCell,
+    TableBody,
+    TableRow,
+    Typography,
+    Paper,
+    Link
 } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
 import UploadIcon from '@mui/icons-material/Upload'
@@ -48,7 +57,6 @@ const CommentForm = ({ handleSubmit }) => {
         } else {
             setError(true)
         }; 
-
     };
 
     const hadleFileUploader = (e) => {
@@ -71,10 +79,7 @@ const CommentForm = ({ handleSubmit }) => {
                     : <></>
                 }
             </Box>
-            <Box
-                textAlign="center"
-                sx={{ m: 0.5 , diplay: "flex", flexDirection:"column"}}
-            >
+            <Box textAlign="center" sx={{ m: 0.5 , diplay: "flex", flexDirection:"column"}}>
                 {screenshots.map((image, index) => {
                     return( 
                             <Chip
@@ -84,7 +89,6 @@ const CommentForm = ({ handleSubmit }) => {
                                 value={index}
                                 onDelete={() => handleFileDeleter(index)}
                             />
-                        
                     )
                 })}
                 
@@ -99,7 +103,6 @@ const CommentForm = ({ handleSubmit }) => {
                             marginRight: "30px",
                         }}
                     >       
-
                             <TextField 
                                 fullWidth
                                 variant="filled"
@@ -107,10 +110,7 @@ const CommentForm = ({ handleSubmit }) => {
                                 value={message}
                                 onChange={(e) => { setMessage(e.target.value )}}
                             />
-                            <Button 
-                                variant="contained" color="success" size="small"
-                                type="file" component="label"
-                            >
+                            <Button variant="contained" color="success" size="small" type="file" component="label">
                                 <input type="file" hidden onChange={hadleFileUploader}/>
                                 <UploadIcon />
                             </Button>
@@ -124,35 +124,87 @@ const CommentForm = ({ handleSubmit }) => {
     )
 }
 
+const Row = ({ comment }) => {
+    return (
+        <TableRow sx={{ '& > *': { borderBottom: 'unset'}, }} >
+            <TableCell component="th" scope="row">
+                <Typography>{comment.user.username}</Typography>
+            </TableCell>
+            <TableCell align="left" colSpan={4}>
+                <Typography>{comment.message}</Typography>
+            </TableCell>
+            <TableCell align="center">
+                <Typography>
+                    {comment.screenshots.length ? 
+                        comment.screenshots.map((url, index) => {
+                            return(
+                                <Link
+                                    key={index}
+                                    href={url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    style={{ color: "inherit"}} underline="hover"
+                                >
+                                    {index+1}
+                                </Link>
+                            )
+                        })
+                        :"None"
+                    }
+                </Typography>
+            </TableCell>
+            <TableCell align="left">
+                <Typography>{detailedDate(comment.date)}</Typography>
+            </TableCell>
+        </TableRow>
+    )
+}
+
 export const IssueComments = ({ comments, handleSubmit }) => {
     return (
-        <Box
-            sx={{
-                marginTop: "30px",
-            }}
+        <Box sx={{marginTop: "30px" }}>
             
-        >
-            <Card raised={true} 
+            <TableContainer component={Paper} raised={true}
                 sx={{ 
-                    maxHeight: 415,
-                    backgroundColor:"#c4c1c1"
+                    height: 455,
+                    backgroundColor:"#c4c1c1",
+                    Width: "100%"
                 }}
             >
-                {comments.slice(0).reverse().map((comment, index) => {
-                    const msg = comment.message
-                    return (
-                        <div key={index}>{msg} /{index+1}</div>
-                    )
-                })}
-            </Card>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center" colSpan={7}>
+                                <Typography><b>Issue Comments</b></Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="left">
+                                <Typography><b>User</b></Typography>
+                            </TableCell>
+                            <TableCell align="left" colSpan={4}> 
+                                <Typography><b>Comment</b></Typography>
+                            </TableCell>
+                            <TableCell align="left">
+                                <Typography><b>Images</b></Typography>
+                            </TableCell>
+                            <TableCell align="left">
+                                <Typography><b>Images</b></Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {comments.slice(0).reverse().map((comment, index) => {
+                            return (
+                                <Row key={index} comment={comment} />
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
 
             <Card raised={true} sx={{marginTop: "30px"}}>
-                <CardContent
-                    sx={{
-                        backgroundColor: "#dbd7cc",
-                        
-                    }}
-                >
+                <CardContent sx={{backgroundColor: "#dbd7cc",}}>
                     <CommentForm handleSubmit={handleSubmit}/>
                 </CardContent>
             </Card>
