bb962635837c273f4168a07691e65a8ac68c75b9
Mateo <madiazt@unal.edu.co>
2022-05-03 17:38:00 -0500

    added basic rendering card on UserDetails comp

--- src/routes/UserDetails.js
+++ src/routes/UserDetails.js
@@ -11,8 +11,17 @@ import {
     Grid,
     Typography,
     Card,
-    Button
+    CardContent,
+    CardActions,
+    Button,
+    Stack
 } from "@mui/material";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import EmailIcon from '@mui/icons-material/Email';
+import BadgeIcon from '@mui/icons-material/Badge';
+import GroupsIcon from '@mui/icons-material/Groups';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 
 // app comp
 import { LoadingPage } from "../components/LoadingPage";
@@ -63,6 +72,85 @@ export const UserDetails = () => {
             <LoadingPage />
         )
     } else if (authorized === true) {
-        <>user displayed info</>
+        return(
+            <Box
+                fullWidth
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    height: "100vh",
+                    m: 5
+                }}
+            >
+                <Card raised={true} 
+                    sx={{
+                        width: "1000px"
+                    }}
+                >
+                    <CardContent
+                        sx={{ 
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            backgroundColor: "#c4c1c1",
+                        }}
+                    >
+                        <AccountBoxIcon size="large"/>
+                        <Typography variant="h3">{userInfo.user.username.toUpperCase()}</Typography>
+                        <Grid container spacing={3} sx={{m:3}}>
+                            <Grid 
+                                item xs={6}
+                                sx={{ display:"flex", flexDirection: "column"}}
+                            >
+                                <Stack direction="row" alignItems="center" gap={2}>
+                                    <BadgeIcon size="large"/>
+                                    <Typography variant="h6"><b>User role</b></Typography>
+                                </Stack>
+                                <Typography>{userInfo.user.role}</Typography>
+                                
+                                <Stack direction="row" alignItems="center" gap={2}>
+                                    <EmailIcon size="large"/>
+                                    <Typography variant="h6"><b>Contact email</b></Typography>
+                                </Stack>
+                                <Typography>{userInfo.user.email}</Typography>
+                            </Grid>
+
+                            <Grid 
+                                item xs={6} 
+                                sx={{ display:"flex", flexDirection: "column"}}
+                            >
+                                <Stack direction="row" alignItems="center" gap={3}>
+                                    <GroupsIcon size="large"/>
+                                    <Typography variant="h6"><b>Project user is part </b></Typography>
+                                    <Typography variant="h5">{userInfo.projects.number}</Typography>
+                                </Stack>
+                                
+
+                                <Stack direction="row" alignItems="center" gap={3}>
+                                    <BugReportIcon size="large"/>
+                                    <Typography variant="h6"><b>Tickets taken</b></Typography>
+                                    <Typography variant="h5">{userInfo.issues.number}</Typography>
+                                </Stack>
+
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                    {(userInfo.user.role !== "Admin" && user.user.role === "Admin") ? 
+                        <CardActions sx={{ backgroundColor: "#838383", display:"flex", flexDirection:"column", alignItem:"center"}}>
+                            <Button 
+                                color="warning"
+                                size="large"
+                                variant="contained"
+                                startIcon={<EngineeringIcon />}
+                            >
+                                make admin
+                            </Button>
+                        </CardActions>
+                    : <></>
+                    }
+                </Card>
+            </Box>
+        )
     }
 }
