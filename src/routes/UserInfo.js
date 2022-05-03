a22145dd90a77cfa71d66c024c373ab3046f6065
Mateo <madiazt@unal.edu.co>
2022-05-03 18:56:51 -0500

    added skeleton of component

--- src/routes/UserInfo.js
+++ src/routes/UserInfo.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+// redux comp
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+
+
 
 export const UserInfo = () => {
-    return (
-        <div>
-            todo...
-        </div>
-    )
+    const user = useSelector(selectUser);
+    let navigate = useNavigate();
+
+    if (!user) {
+        navigate("/")
+    } else {
+        return (
+            <>todo...</>
+        )
+    }
 }
