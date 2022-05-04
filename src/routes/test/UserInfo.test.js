1fa0e0ea3dc16af09ab4235e175b7b1d27a3d00f
Mateo <madiazt@unal.edu.co>
2022-05-03 19:03:57 -0500

    added skeleton of basic rendered element of UserInfo

---
+++ src/routes/test/UserInfo.test.js
@@ -0,0 +1,8 @@
+
+//test component
+import { UserInfo } from "../UserInfo";
+
+describe("UserInfo component", () => {
+    test.todo("handles no logged user");
+    test.todo("renders user info card");
+})
