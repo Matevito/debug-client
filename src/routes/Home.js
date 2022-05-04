1012b31223b79f682e7252d458523a1ac32378b3
Mateo <madiazt@unal.edu.co>
2022-05-04 13:25:45 -0500

    added skeleton of route test-suite

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
 // redux state management
 import { useSelector } from "react-redux";
@@ -7,81 +7,65 @@ import { selectUser } from "../features/userSlice";
 
 // mui comp
 import {
-    Button
+    Box
 } from "@mui/material";
 
 // Home components
 import { Welcome } from "../components/Welcome";
+import { LoadingPage } from "../components/LoadingPage";
 
-const CreateProjectLink = () => {
-    return (
-        <Link to="project/create" style={{ color: "inherit", textDecoration: 'none' }}>
-            <Button color="warning">
-                    Create project
-            </Button>
-        </Link>
-    )
-};
+// api comp
+import api from "../features/api";
 
 export const Home = () => {
     const user = useSelector(selectUser);
+    let navigate = useNavigate();
+
+    const [authorized, setAuthorized] = useState(null);
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        async function getProjectList() {
+            const config = {
+                headers: { "auth-token": user.token }
+            };
+            const url = "/project/list";
+
+            try {
+                const listRes = await api.get(url, config);
+                setProjectList(listRes.data.data);
+            } catch(err) {
+                setAuthorized(false);
+                navigate("/protected-route");
+            }
+        };
+
+        if (user){
+            if (user.user.role !== "Admin"){
+                setAuthorized(true)
+            } else {
+                // fetch projectList info for admin view!
+                getProjectList()
+            }
+        }
+    }, [user, navigate])
+
     if (!user) {
         return(
             <Welcome />
         )
-    } else if (user.user.role === "Admin"){
-        return(
-            <>
-            <span> loged as{user.user.role}</span>
-            <CreateProjectLink />
-            {user.projects.list.map((proj, i) => {
-                return (
-                    <div key={i}>
-                    <Link to={`/project/${proj._id}/issue/create`} style={{ color: "inherit", textDecoration: 'none' }}>
-                        <Button>
-                            Create issue on: {proj.title}
-                        </Button>
-                    </Link>
-                    </div>
-                )
-            })}
-            <h1>Projects on db(todo this)</h1>
-            {user.projects.list.map((proj, i) => {
-                return(
-                    <div key={i}>
-                        <Link to={`project/${proj._id}`}>
-                            {proj.title}
-                        </Link>
-                    </div>
-                )
-            })}
-            </>
-        );
-    } else {
+    } else if (authorized === false){
+        return (
+            <LoadingPage />
+        )
+    } else if (authorized === true) {
         return (
             <>
-            <span>no admin</span>
-            {user.projects.list.map((proj, i) => {
-                return (
-                    <div key={i}>
-                    <Link to={`/project/${proj._id}/issue/create`} style={{ color: "inherit", textDecoration: 'none' }}>
-                        <Button>
-                            Create issue on: {proj.title}
-                        </Button>
-                    </Link>
-                    </div>
-                )
-            })}
-            <h1>Projects</h1>
-            {user.projects.list.map((proj, i) => {
-                return(
-                    <div key={i}>
-                        <Link to={`project/${proj._id}`}>
-                            {proj.title}
-                        </Link>
-                    </div>
-                )
-            })}
+            {user.user.role === "Admin" ? <>only admin</> : <></>}
+            {user.user.role !== "Developer" ? <>team leader and admin</> : <></>}
+    
+            <span>user projects</span>
+            <span>user info</span>u
             </>
         )
     }
---
+++ src/routes/test/Home.test.js
@@ -0,0 +1,10 @@
+describe("Home component", () => {
+    // auth tests
+    test.todo("handles user not logged-in");
+    test.todo("handles error fetching admin user");
+
+    // functional tests
+    test.todo("renders admin user tables");
+    test.todo("render team leader table");
+    test.todo("handles developer");
+})
