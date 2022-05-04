536271acb5903288f2991efb3b5024e2c36b288c
Mateo <madiazt@unal.edu.co>
2022-05-04 13:35:19 -0500

    added error fetching admin data test

--- src/routes/test/Home.test.js
+++ src/routes/test/Home.test.js
@@ -1,10 +1,74 @@
+import { Provider } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+// testing comps
+import { render, screen, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+// test component
+import { Home } from "../Home";
+
+// mock functions
+import { getData } from "../../features/mockedUserStores";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const rootAPI = "https://pure-falls-26749.herokuapp.com/apiv1";
+
+const server = setupServer(
+    rest.get("*", (req, res, ctx) => {
+        return res(ctx.status(400), ctx.json({ error: "error fetching data"}))
+    })
+);
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
+const renderComponent = (store) => {
+    return render(
+        <Provider store={store}>
+            <MemoryRouter initialEntries={["/"]}>
+                <Routes>
+                    <Route 
+                        path="/"
+                        element={<Home />}
+                    />
+                </Routes>
+            </MemoryRouter>
+        </Provider>
+    )
+}
 describe("Home component", () => {
+    let noLoggedUser;
+    let devStore;
+    let teamLeaderStore;
+    let adminStore;
+
+    beforeEach(() => {
+        const usersData = getData();
+        noLoggedUser = usersData[0];
+        adminStore = usersData[3];
+    })
+    // required to mock rest-api server
+    beforeAll(() => server.listen())
+    afterEach(() => server.resetHandlers());
+    afterAll(() => {
+        server.close()
+        jest.clearAllMocks();
+    })
+
     // auth tests
     test.todo("handles user not logged-in");
-    test.todo("handles error fetching admin user");
+    test("handles error fetching admin user", async() => {
+        renderComponent(adminStore);
+        await waitFor(() => mockedUsedNavigate.mock.lastCall[0]);
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/protected-route")
+    });
 
     // functional tests
-    test.todo("renders admin user tables");
-    test.todo("render team leader table");
     test.todo("handles developer");
+    test.todo("render team leader table");
+    test.todo("renders admin user tables");
 })
