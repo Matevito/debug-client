d2bc2273c91dd207b1508cc52412fc719d834336
Mateo <madiazt@unal.edu.co>
2022-05-03 19:14:01 -0500

    added UserInfo tested comp

--- src/routes/test/UserInfo.test.js
+++ src/routes/test/UserInfo.test.js
@@ -1,8 +1,53 @@
+import { Provider } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+// testing comps
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
 
 //test component
 import { UserInfo } from "../UserInfo";
 
+// mock functs
+import { getData } from "../../features/mockedUserStores";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
+const renderComponent = (url, store) => {
+    return render(
+        <Provider store={store}>
+            <MemoryRouter initialEntries={[url]}>
+                <Routes>
+                    <Route 
+                        path="/user/info"
+                        element={<UserInfo />}
+                    />
+                </Routes>
+            </MemoryRouter>
+        </Provider>
+    )
+}
 describe("UserInfo component", () => {
-    test.todo("handles no logged user");
-    test.todo("renders user info card");
+    let noLoggedUser;
+    let devStore;
+    beforeEach(() => {
+        const usersData = getData();
+        noLoggedUser = usersData[0];
+        devStore = usersData[1];
+    });
+    afterAll(() => {
+        jest.clearAllMocks();
+    })
+    test("handles no logged user", () => {
+        renderComponent("/user/info", noLoggedUser)
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/")
+});
+    test("renders user info card", () => {
+        const { container } = renderComponent("/user/info", devStore);
+        expect(container).toMatchSnapshot();
+    });
 })
