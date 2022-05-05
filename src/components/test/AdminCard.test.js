5593b88ff2a4b3375c1e9a7676c013390a0805af
Mateo <madiazt@unal.edu.co>
2022-05-04 22:43:01 -0500

    added AdminCard comp test-suite

--- src/components/test/AdminCard.test.js
+++ src/components/test/AdminCard.test.js
@@ -1,4 +1,32 @@
+import { MemoryRouter } from "react-router-dom";
+// testing comps
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+// tested component
+import { AdminCard } from "../AdminCard";
+
 describe("AdminCard component", () => {
-    test.todo("comp deals with blank array");
-    test.todo("comp renders correct card")
+    test("comp deals with blank array", () => {
+        render(<AdminCard projects={[]}/>, {wrapper: MemoryRouter})
+        expect(screen.getByText("There are currently 0 projects on db.")).toBeInTheDocument()
+        expect(screen.getByText("There are no projects currently on db. Why not create one?")).toBeInTheDocument()
+        expect(screen.getByText("create a project")).toBeInTheDocument()
+    });
+    test("comp renders correct card", () => {
+        const projectList = [
+            {
+                _id: "1",
+                title: "proj1",
+                team: [],
+                teamLeader: "adminUser",
+                issues: 0,
+                solvedIssues: 0
+            }
+        ]
+        render(<AdminCard projects={projectList}/>, {wrapper: MemoryRouter})
+        expect(screen.getByText("There are currently 1 projects on db.")).toBeInTheDocument()
+        expect(screen.getByText("PROJECTS ON APP")).toBeInTheDocument()
+        expect(screen.getByText("create a project")).toBeInTheDocument()
+    })
 })
